96314f16081063bc26cc5be34461693a1dfae330
이우주 <mm9326@naver.com>
2023-08-02 17:55:48 +0900

    step

--- SStice_230801.py
+++ SStice_230801.py
@@ -48,5 +48,5 @@
 # list[0] = list[0] - 1
 # print(list)
 
-for i in range(9,0,-1):
-    print(i)
+# for i in range(9,0,-1):
+#     print(i)
--- daily_230801.py
+++ daily_230801.py
@@ -2,55 +2,55 @@
 
 # 1208. Flatten
 
-T = 10
-for tc in range(1, T+1):
-    N = int(input())
-    arr = list(map(int,input().split()))
+# T = 10
+# for tc in range(1, T+1):
+#     N = int(input())
+#     arr = list(map(int,input().split()))
 
-    for idx in range(N):    # 1번 할때마다
-        max_value = arr[0]  # arr의 최댓값 초기 설정
-        min_value = arr[0]  # arr의 최솟값 초기 설정
-        idx_max = 0         # 최댓값의 인덱스 초기 설정
-        idx_min = 0         # 최솟값의 인덱스 초기 설정
+#     for idx in range(N):    # 1번 할때마다
+#         max_value = arr[0]  # arr의 최댓값 초기 설정
+#         min_value = arr[0]  # arr의 최솟값 초기 설정
+#         idx_max = 0         # 최댓값의 인덱스 초기 설정
+#         idx_min = 0         # 최솟값의 인덱스 초기 설정
 
-        for i in range(len(arr)):
-            if max_value < arr[i]:
-                max_value = arr[i]
-                idx_max = i             # 최댓값과 그때의 인덱스를 추출
-            else:
-                continue
+#         for i in range(len(arr)):
+#             if max_value < arr[i]:
+#                 max_value = arr[i]
+#                 idx_max = i             # 최댓값과 그때의 인덱스를 추출
+#             else:
+#                 continue
 
-        for j in range(len(arr)):
-            if min_value > arr[j]:
-                min_value = arr[j]
-                idx_min = j             # 최솟값과 그때의 인덱스를 추출
-            else:
-                continue
+#         for j in range(len(arr)):
+#             if min_value > arr[j]:
+#                 min_value = arr[j]
+#                 idx_min = j             # 최솟값과 그때의 인덱스를 추출
+#             else:
+#                 continue
 
-        arr[idx_max] -= 1               # 최댓값일 때의 arr의 인덱스로 가서 그 값을 -1
-        arr[idx_min] += 1               # 최솟값일 때의 arr의 인덱스로 가서 그 값을 +1
+#         arr[idx_max] -= 1               # 최댓값일 때의 arr의 인덱스로 가서 그 값을 -1
+#         arr[idx_min] += 1               # 최솟값일 때의 arr의 인덱스로 가서 그 값을 +1
 
-    def Max_Min(my_arr):
-        my_max = 0
-        my_min = my_arr[0]
-        for i in range(len(my_arr)):
-            if my_max < my_arr[i]:
-                my_max = my_arr[i]
-            else:
-                continue
-        for j in range(len(my_arr)):
-            if my_min > my_arr[j]:
-                my_min = my_arr[j]
-            else:
-                continue
+#     def Max_Min(my_arr):
+#         my_max = 0
+#         my_min = my_arr[0]
+#         for i in range(len(my_arr)):
+#             if my_max < my_arr[i]:
+#                 my_max = my_arr[i]
+#             else:
+#                 continue
+#         for j in range(len(my_arr)):
+#             if my_min > my_arr[j]:
+#                 my_min = my_arr[j]
+#             else:
+#                 continue
 
-        result = my_max - my_min      # 덤프가 모두 끝난 후 arr의 최댓값 및 최솟값의 차를 구함
+#         result = my_max - my_min      # 덤프가 모두 끝난 후 arr의 최댓값 및 최솟값의 차를 구함
 
-        return result
+#         return result
 
 
 
-    print(f'#{tc} {Max_Min(arr)}')
+#     print(f'#{tc} {Max_Min(arr)}')
 
 
 
@@ -84,10 +84,15 @@ for tc in range(1, T+1):
 
 # 18058. 4831. 전기버스
 
+# T = int(input())
+# for tc in range(1,T+1):
+#     K, N, M = map(int,input().split())
+#     arr = list(map(int,input().split()))
+    
+#     print (K, N, M)
 
 
-
-
+# N - my_max > K 
 
 
 
@@ -103,6 +108,21 @@ for tc in range(1, T+1):
 
 # 9386. 연속한 1의 개수
 
+# T = int(input())
+# for tc in range(1, T+1):
+#     N = int(input())
+#     arr = list(map(int, input()))
 
+#     num = 0
+#     max_num = 0
+#     for i in range(N):
+#         if arr[i] == 1:
+#             num += 1
+#             if max_num < num:
+#                 max_num = num
+#         else:
+#             num = 0
+
+#     print(f'#{tc} {max_num}')
 
 
